871db0d7011d7fab404cbe01bc4fd18908748dea
vishwajeet1207 <vishwajeetdeshmukh1107@gmail.com>
2024-06-18 15:32:47 +0530

    first

---
+++ 01_Auto_Increment/src/App.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+
+
+
+function App(){
+  const [count,setCount]=useState(0);
+const handleincrease=()=>{
+  setCount(count + 1);
+}
+
+  return(
+    <div>
+      <h1>Hii</h1>
+      <h1>Count:{count}</h1>
+      <button onClick={handleincrease}>Increase</button>
+    </div>
+  )
+}
+
+export default  App;
