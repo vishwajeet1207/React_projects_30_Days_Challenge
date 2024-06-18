fa17acdf25e4efb73f37c1329f579edf09ac0e52
vishwajeet1207 <vishwajeetdeshmukh1107@gmail.com>
2024-06-18 16:19:22 +0530

    update

--- 01_Count_Increment/src/App.jsx
+++ 01_Count_Increment/src/App.jsx
@@ -7,11 +7,15 @@ function App(){
 const handleincrease=()=>{
   setCount(count + 1);
 }
+const handleDecrease=()=>{
+  setCount(count -1);
+}
 
   return(
     <div>
       <h1>Count:{count}</h1>
       <button onClick={handleincrease}>Increase</button>
+      <button onClick={handleDecrease}>Decrease</button>
     </div>
   )
 }
